3cd7d4fb7d057c086480d0d49c9f3bddd34ee3bf
Davix <sdavid4815@gmail.com>
2024-03-13 00:25:29 -0300

    ejercicios 8 al 10 bash scripting

---
+++ Labs-main/T01 - Nivelacion/Obligatorias/01-Shell-Bash-Scripting/ejercicio10.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#genero numero random y lo guardo en el archivo ejercicio10.txt
+
+numeroRandom=$RANDOM
+echo $numeroRandom
+echo $numeroRandom >> ejercicio10.txt
---
+++ Labs-main/T01 - Nivelacion/Obligatorias/01-Shell-Bash-Scripting/ejercicio9.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+read -p "Ingrese el directorio del cual quiere contar los archivos: " directorio
+
+# creo funcion
+contar_archivos ()
+{
+#cuento archivos y directorios en el directorio actual
+#ls para listar archivos y directorios 
+#con el pipe | hago que el stdout de ls sea el stdin para wc -l
+#wc(es para contar) -l(el -l indica que se cuentan lineas)
+#totalArchivos=$(ls | wc -l)
+
+#ls para listar ** -p para agregar / al final del nombre de cada directorio
+#con el pipe | llega el stdout de ls -p a grep como stdin
+#grep busca en lo que llego al agregarle -v busca por la negativa es decir los archivos que no contengan / que es lo que se agrego al momento de hacer el ls por poner el -p
+totalArchivos=$(ls -p $directorio | grep -v /| wc -l)
+echo "El directorio \"$directorio\" contiene: $totalArchivos archivos. "
+}
+
+contar_archivos
